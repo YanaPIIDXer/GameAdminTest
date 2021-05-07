11e68f2b4548218c26ea40ddec9add80411f5b04
YanaPIIDXer <>
2021-05-08 08:19:40 +0900

    バージョン管理に含めるべきでない値を定義するための仕組み

---
+++ GameClient/Assets/Scripts/EnvironmentDatas.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// バージョン管理に含めるべきではないデータ定義
+/// </summary>
+public interface IEnvironmentData
+{
+    /// <summary>
+    /// アプリケーションキー
+    /// </summary>
+    string ApplicationKey { get; }
+
+    string ClientKey { get; }
+}
+
+/// <summary>
+/// バージョン管理に含めるべきではないデータへのアクセス
+/// </summary>
+public static class EnvironmentDatas
+{
+    /// <summary>
+    /// IEnvironmentData実装オブジェクト
+    /// </summary>
+    public static IEnvironmentData Env { set; private get; }
+
+    /// <summary>
+    /// アプリケーションキー
+    /// </summary>
+    public static string ApplicationKey { get { return Env.ApplicationKey; } }
+
+    /// <summary>
+    /// クライアントキーs
+    /// </summary>
+    /// <value></value>
+    public static string ClientKey { get { return Env.ClientKey; } }
+
+}
