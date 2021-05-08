426d9c6da0fbfcc67c7e3ffc5752b466883877a4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-08 13:08:57 +0900

    とりあえず課金アイテムのリストを取得するメソッドを定義

---
+++ GameClient/Assets/Scripts/APICall.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// 課金アイテムデータ
+/// </summary>
+public struct PayItem
+{
+    /// <summary>
+    /// ID
+    /// </summary>
+    public string Id;
+
+    /// <summary>
+    /// 名前
+    /// </summary>
+    public string Name;
+
+    /// <summary>
+    /// 値段
+    /// </summary>
+    public int Price;
+}
+
+/// <summary>
+/// APIの呼び出し
+/// </summary>
+public static class APICall
+{
+    /// <summary>
+    /// エンドポイント
+    /// </summary>
+    private static readonly string Endpoint = "http://localhost/api/";
+
+    /// <summary>
+    /// 課金アイテムリスト取得
+    /// </summary>
+    /// <param name="Callback">コールバック</param>
+    public static IEnumerator GetPayItemList(Action<List<PayItem>> Callback)
+    {
+        using (var Req = UnityWebRequest.Get(Endpoint + "pay_item_list.php"))
+        {
+            yield return Req.SendWebRequest();
+            Debug.Log(Req.downloadHandler.text);
+        }
+    }
+}
---
+++ GameClient/Assets/Scripts/GetListButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 課金アイテムリスト取得ボタン
+/// </summary>
+public class GetListButton : MonoBehaviour
+{
+    /// <summary>
+    /// 押された
+    /// </summary>
+    public void OnPressed()
+    {
+        StartCoroutine(APICall.GetPayItemList(null));
+    }
+}
