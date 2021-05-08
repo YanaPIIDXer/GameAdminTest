2efef2e5abd5b5e6de82046ed8b0da5ec22f9e4b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-09 03:21:10 +0900

    Prefabの読み込みとか

--- GameClient/Assets/Scripts/GetListButton.cs
+++ GameClient/Assets/Scripts/GetListButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 /// <summary>
 /// 課金アイテムリスト取得ボタン
@@ -15,16 +16,30 @@ public class GetListButton : MonoBehaviour
     private GameObject TargetView = null;
 
     /// <summary>
+    /// 要素のPrefab
+    /// </summary>
+    private static GameObject ItemPrefab = null;
+
+    /// <summary>
     /// 押された
     /// </summary>
     public void OnPressed()
     {
+        while (TargetView.transform.childCount > 0)
+        {
+            var Child = TargetView.transform.GetChild(0);
+            GameObject.Destroy(Child);
+        }
+        TargetView.transform.DetachChildren();
+
+        if (ItemPrefab == null)
+        {
+            ItemPrefab = Resources.Load<GameObject>("Prefabs/PayItemButton");
+            Debug.Assert(ItemPrefab != null);
+        }
+
         StartCoroutine(APICall.GetPayItemList((Items) =>
         {
-            foreach (var Item in Items)
-            {
-                Debug.Log(Item.name + " Price:" + Item.price);
-            }
         }));
     }
 }
