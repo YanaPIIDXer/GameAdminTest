ab75e065a35b7acff9c456250a84c788aff0615f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-08 12:13:24 +0900

    DBからデータを拾うようにした

--- www/html/api/pay_item_list.php
+++ www/html/api/pay_item_list.php
@@ -1,7 +1,19 @@
 <?php
-    // TODO:DBから引っ張ってくる
-    $items = [
-        ["id" => "wabi_1000", "name" => "詫び石1000個", "price" => 610]
-    ];
+    $items = [];
+    try
+    {
+        //                                               ↓良い子はマネしないでね
+        $pdo = new PDO("mysql:dbname=test_game;host=db", "root", "secret");
+        $stmt = $pdo->query("select * from pay_items;");
+        while ($result = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            array_push($items, ["id" => $result["id"], "name" => $result["name"], "price" => $result["price"]]);
+        }
+    }
+    catch (PDOException $e)
+    {
+        print("PDOException! " . $e->getMessage());
+        die();
+    }
     echo json_encode($items);
 ?>
